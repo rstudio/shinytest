cebd43246c0f0d0f6d6901f3b3a385fab1c5dd07
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-31 14:47:34 +0100

    Update tests look more like tests
    expect_update is still not a proper expectation yet.

--- tests/testthat/test-updates.R
+++ tests/testthat/test-updates.R
@@ -5,9 +5,14 @@ test_that("simple updates", {
 
   app <- shinyapp$new("apps/050-kmeans-example")
 
-  ## Not a real test yet
-  app$expect_update(xcol = "Sepal.Width", output = "plot1")
-  app$expect_update(ycol = "Petal.Width", output = "plot1")
-  app$expect_update(clusters = 4, output = "plot1")
-
+  ## Not a real expectation yet
+  expect_true(
+    app$expect_update(xcol = "Sepal.Width", output = "plot1")
+  )
+  expect_true(
+    app$expect_update(ycol = "Petal.Width", output = "plot1")
+  )
+  expect_true(
+    app$expect_update(clusters = 4, output = "plot1")
+  )
 })
