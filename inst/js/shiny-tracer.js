a86fe88dcd3019df2bc6609a29435cc89a2bc45d
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-31 10:29:43 +0100

    Tracer: log shiny:busy and shiny:idle messages

--- inst/js/shiny-tracer.js
+++ inst/js/shiny-tracer.js
@@ -1,14 +1,33 @@
 
+function shinytest_create_store() {
+    if (!window.shinytest) {
+	window.shinytest = {
+	    connected: true,
+	    busy: null,
+	    log_entries: [],
+	    log: function(message) {
+		window.shinytest.log_entries.push({
+		    timestamp: new Date(),
+		    message: message
+		})
+	    }
+	};
+    }
+}
+
 $(document).on("shiny:connected", function(e) {
-    window.shinytest = {
-	connected: true,
-	log_entries: [],
-	log: function(message) {
-	    window.shinytest.log_entries.push({
-		timestamp: new Date(),
-		message: message
-	    })
-	}
-    };
+    shinytest_create_store();
     window.shinytest.log("connected");
-})
+});
+
+$(document).on("shiny:busy", function(e) {
+    shinytest_create_store();
+    window.shinytest.busy = true;
+    window.shinytest.log("busy");
+});
+
+$(document).on("shiny:idle", function(e) {
+    shinytest_create_store();
+    window.shinytest.busy = false;
+    window.shinytest.log("idle");
+});
