9c7f623875f444e8b510c6e191ed06c0ff48730f
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-05 07:48:06 +0100

    Test checkboxInput & checkboxGroupInput triggered updates

--- tests/testthat/test-updates.R
+++ tests/testthat/test-updates.R
@@ -1,6 +1,21 @@
 
 context("updates")
 
+test_that("updates for all widget in the gallery", {
+
+  app <- shinyapp$new("apps/081-widgets-gallery")
+
+  expect_update(app, checkbox = FALSE, output = "checkbox")
+  expect_equal(app$get_value("checkbox", "output"), "[1] FALSE")
+  expect_update(app, checkbox = TRUE, output = "checkbox")
+  expect_equal(app$get_value("checkbox", "output"), "[1] TRUE")
+
+  expect_update(app, checkGroup = c("1", "3"), output = "checkGroup")
+  expect_equal(app$get_value("checkGroup"), c("1", "3"))
+  expect_update(app, checkGroup = c("2"), output = "checkGroup")
+  expect_equal(app$get_value("checkGroup"), c("2"))
+})
+
 test_that("simple updates", {
 
   app <- shinyapp$new("apps/050-kmeans-example")
