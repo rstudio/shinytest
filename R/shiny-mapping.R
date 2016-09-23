abf08ed11efd0be8ca83d65b7f71d52d9e35a0d0
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-23 23:16:34 +0100

    Rewrite find_widget method to speed up, simplify

--- R/shiny-mapping.R
+++ R/shiny-mapping.R
@@ -12,132 +12,69 @@
 
 app_find_widget <- function(self, private, name, iotype) {
 
-  el <- self$find_elements(css = paste0("#", name))[[1]]
-  el_input <- tryCatch(
-    self$find_elements(css = paste0("#", name, ".shiny-bound-input"))[[1]],
-    error = function(e) NULL
-  )
-  el_output <-tryCatch(
-    self$find_elements(css = paste0("#", name, ".shiny-bound-output"))[[1]],
-    error = function(e) NULL
-  )
+  css <- if (iotype == "auto") {
+    paste0("#", name)
 
-  res <- if (iotype == "auto") {
-    find_input_widget(self, private, el_input) %||%
-    find_output_widget(self, private, el_output) %||%
-    find_input_widget(self, private, el) %||%
-    find_output_widget(self, private, el)
   } else if (iotype == "input") {
-    find_input_widget(self, private, el_input)
-  } else {
-    find_output_widget(self, private, el_output)
+    paste0("#", name, ".shiny-bound-input")
+
+  } else if (iotype == "output") {
+    paste0("#", name, ".shiny-bound-output")
   }
 
-  if (is.null(res)) {
-    stop("Cannot find shiny input/output widget ", sQuote(name))
+  els <- self$find_elements(css = css)
+
+  if (length(els) == 0) {
+    stop(
+      "Cannot find ",
+      if (iotype != "auto") paste0(iotype, " "),
+      "widget ", name
+    )
+
+  } else if (length(els) > 1) {
+    warning(
+      "Multiple ",
+      if (iotype != "auto") paste0(iotype, " "),
+      "widgets with id ", name
+    )
   }
+
+  type <- els[[1]]$execute_script(
+    "var el = $(arguments[0]);
+     if (el.data('shinyInputBinding') !== undefined) {
+       return ['input', el.data('shinyInputBinding').name];
+     } else {
+       var name = el.data('shinyOutputBinding').binding.name;
+       if (name == 'shiny.textOutput' && el[0].tagName == 'PRE') {
+         return ['output', 'shiny.verbatimTextOutput'];
+       } else {
+         return ['output', name];
+       }
+     }"
+  )
+
+  ## We could use the JS names as well, but it is maybe better to use
+  ## the names the users encounter with in the Shiny R docs
+  widget_names <- c(
+    "shiny.actionButtonInput" = "actionButton",
+    "shiny.checkboxInput" = "checkboxInput",
+    "shiny.checkboxGroupInput" = "checkboxGroupInput",
+    "shiny.dateInput" = "dateInput",
+    "shiny.dateRangeInput" = "dateRangeInput",
+    "shiny.fileInputBinding" = "fileInput",
+    "shiny.numberInput" = "numericInput",
+    "shiny.radioInput" = "radioButtons",
+    "shiny.selectInput" = "selectInput",
+    "shiny.sliderInput" = "sliderInput",
+    "shiny.textInput" = "textInput",
+    "shiny.verbatimTextOutput" = "verbatimTextOutput",
+    "shiny.passwordInput" = "passwordInput"
+  )
 
   widget$new(
     name = name,
-    element = res$element,
-    type = res$type,
-    iotype = res$iotype
+    element = els[[1]],
+    type = unname(widget_names[type[[2]]]),
+    iotype = type[[1]]
   )
-}
-
-find_input_widget <- function(self, private, el) {
-
-  if (is.null(el)) return(NULL)
-
-  tag <- el$get_name()
-  type <- el$get_attribute("type")
-  class <- parse_class(el$get_attribute("class"))
-
-  e <- function(name) {
-    list(element = el, iotype = "input", type = name)
-  }
-
-  ## If a <button>, then it is an actionbutton
-  if (tag == "button") return(e("actionButton"))
-
-  ## Only checkboxInput has type "checkbox"
-  if (tag == "input" && type == "checkbox") {
-    return(e("checkboxInput"))
-  }
-
-  ## checkboxGroupInput has specific class
-  if (tag == "div" && "shiny-input-checkboxgroup" %in% class) {
-    return(e("checkboxGroupInput"))
-  }
-
-  ## dateInput has class
-  if (tag == "div" && "shiny-date-input" %in% class) {
-    return(e("dateInput"))
-  }
-
-  ## dateRangeInput has class
-  if (tag == "div" && "shiny-date-range-input" %in% class) {
-    return(e("dateRangeInput"))
-  }
-
-  ## fileInput has type "file"
-  if (tag == "input" && type == "file") return(e("fileInput"))
-
-  ## numericInput is <input> type number
-  if (tag == "input" && type == "number") return(e("numericInput"))
-
-  ## radioButtons has a class
-  if (tag == "div" && "shiny-input-radiogroup" %in% class) {
-    return(e("radioButtons"))
-  }
-
-  ## If tag is <select> it is a selectInput
-  if (tag == "select") return(e("selectInput"))
-
-  ## sliderInput has class js-range-slider
-  if (tag == "input" && "js-range-slider" %in% class) {
-    return(e("sliderInput"))
-  }
-
-  ## textInput has type
-  if (tag == "input" && type == "text") return(e("textInput"))
-
-  ## passwordInput has type
-  if (tag == "input" && type == "password") return(e("passwordInput"))
-
-  NULL
-}
-
-find_output_widget <- function(self, private, el) {
-
-  if (is.null(el)) return(NULL)
-
-  tag <- el$get_name()
-  type <- el$get_attribute("type")
-  class <- parse_class(el$get_attribute("class"))
-
-  e <- function(name) {
-    list(element = el, iotype = "output", type = name)
-  }
-
-  ## htmlOutput has class
-  if ("shiny-html-output" %in% class) return(e("htmlOutput"))
-
-  ## plotOutput has class
-  if ("shiny-plot-output" %in% class) return(e("plotOutput"))
-
-  ## tableOutput has classes datatables shiny-bound-output
-  if ("datatables" %in% class && "shiny-bound-output" %in% class) {
-    return(e("tableOutput"))
-  }
-
-  ## verbatimTextOutput is <pre> and has class
-  if (tag == "pre" && "shiny-text-output" %in% class) {
-    return(e("verbatimTextOutput"))
-  }
-
-  ## textOutput has the same class, but not <pre> (we assume)
-  if (tag != "pre" && "shiny-text-output" %in% class) {
-    return(e("textOutput"))
-  }
 }
