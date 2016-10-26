77d92142a912ff7b24141fbcb92d78dbcae3ad69
Winston Chang <winston@stdout.org>
2016-10-26 16:12:27 -0500

    Remove unimplemented methods

--- R/app.R
+++ R/app.R
@@ -282,14 +282,7 @@ shinyapp <- R6Class(
 
     upload_file = function(..., wait_ = TRUE, values_ = TRUE, timeout_ = 3000)
       app_upload_file(self, private, ..., wait_ = wait_, values_ = values_,
-                      timeout_ = timeout_),
-
-    get_snapshot = function()
-      app_get_snapshot(self, private),
-
-    get_inputs = function()
-      app_get_inputs(self, private)
-
+                      timeout_ = timeout_)
   ),
 
   private = list(
