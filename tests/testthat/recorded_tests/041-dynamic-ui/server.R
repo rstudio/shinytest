525fd69ed680d1988df0d9feba6f63c308b8d31f
Winston Chang <winston@stdout.org>
2017-08-25 22:17:44 -0500

    Add recorded tests for dynamic UI

---
+++ tests/testthat/recorded_tests/041-dynamic-ui/server.R
@@ -0,0 +1,58 @@
+function(input, output) {
+
+  output$ui <- renderUI({
+    if (is.null(input$input_type))
+      return()
+
+    # Depending on input$input_type, we'll generate a different
+    # UI component and send it to the client.
+    switch(input$input_type,
+      "slider" = sliderInput("dynamic", "Dynamic",
+                             min = 1, max = 20, value = 10),
+      "text" = textInput("dynamic", "Dynamic",
+                         value = "starting value"),
+      "numeric" =  numericInput("dynamic", "Dynamic",
+                                value = 12),
+      "checkbox" = checkboxInput("dynamic", "Dynamic",
+                                 value = TRUE),
+      "checkboxGroup" = checkboxGroupInput("dynamic", "Dynamic",
+        choices = c("Option 1" = "option1",
+                    "Option 2" = "option2"),
+        selected = "option2"
+      ),
+      "radioButtons" = radioButtons("dynamic", "Dynamic",
+        choices = c("Option 1" = "option1",
+                    "Option 2" = "option2"),
+        selected = "option2"
+      ),
+      "selectInput" = selectInput("dynamic", "Dynamic",
+        choices = c("Option 1" = "option1",
+                    "Option 2" = "option2"),
+        selected = "option2"
+      ),
+      "selectInput (multi)" = selectInput("dynamic", "Dynamic",
+        choices = c("Option 1" = "option1",
+                    "Option 2" = "option2"),
+        selected = c("option1", "option2"),
+        multiple = TRUE
+      ),
+      "date" = dateInput("dynamic", "Dynamic"),
+      "daterange" = dateRangeInput("dynamic", "Dynamic")
+    )
+  })
+
+  output$input_type_text <- renderText({
+    # if (identical(isolate(input$input_type), "checkbox")) {
+    #   Sys.sleep(2.5)
+    # }
+    input$input_type
+  })
+
+  output$dynamic_value <- renderPrint({
+    if (identical(isolate(input$input_type), "checkbox")) {
+      Sys.sleep(2.5)
+    }
+    str(input$dynamic)
+  })
+
+}
---
+++ tests/testthat/test-recorded-tests.R
@@ -0,0 +1,30 @@
+context("recorded-tests")
+
+# If a directory has an .Rmd file, return the path with the .Rmd file appended;
+# otherwise return the path unchanged. If there is more than one .Rmd, throw an
+# error.
+append_rmd <- function(path) {
+  files <- dir(path, pattern = ".*\\.Rmd$", full.names = TRUE, ignore.case = TRUE)
+  if (length(files) == 0) {
+    path
+  } else if (length(files) == 1) {
+    files
+  } else {
+    stop("There must be 0 or 1 .Rmd files in dir '", path, "' but there are ",
+      length(files)
+    )
+  }
+}
+
+if (dir.exists("recorded_tests")) {
+  app_dirs <- Filter(dir.exists, dir("recorded_tests", full.names = TRUE))
+  if (length(app_dirs) > 0) {
+    for (app_dir in app_dirs) {
+      # If the dir contains an .Rmd, add that to the path
+      path <- append_rmd(app_dir)
+      test_that(basename(path), {
+        shinytest::expect_pass(shinytest::testApp(path, compareImages = FALSE))
+      })
+    }
+  }
+}
