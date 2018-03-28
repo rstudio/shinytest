62f4266bb9c830a9fbc3e7cca9aa6900fecf03ee
Winston Chang <winston@stdout.org>
2018-03-28 16:23:01 -0500

    Fix function call

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat)
 library(shinytest)
 
 if (Sys.getenv("NOT_CRAN", "") != "" || Sys.getenv("CI", "") != "") {
-  if (!dependenciesInstalled) installDependencies()
+  if (!dependenciesInstalled()) installDependencies()
   message("Using phantom.js from ", shinytest:::find_phantom(), "\n")
   test_check("shinytest")
 }
