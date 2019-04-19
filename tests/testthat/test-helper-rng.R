08f8643e886eba398761a62c36a4accd9199b397
Carson Sievert <cpsievert1@gmail.com>
2019-04-19 12:14:13 -0500

    add note about RNG workaround being temporary

--- tests/testthat/test-helper-rng.R
+++ tests/testthat/test-helper-rng.R
@@ -2,4 +2,6 @@
 # currently rely on randomness and are currently expecting
 # the default RNG kind from R 3.5
 # CRAN has suggested this as workaround for random tests
+# NOTE: this workaround may be removed once we're no longer
+# testing on R 3.5 or earlier
 suppressWarnings(RNGversion("3.5.0"))
