2811034b5c56fd01305fc159d2c917c8054c4f60
Yann Richet <richetyann@gmail.com>
2019-07-28 17:17:37 +0200

    Must fail with "Cannot find shiny port number. shiny"

--- tests/testthat/test-shiny-driver.R
+++ tests/testthat/test-shiny-driver.R
@@ -41,6 +41,6 @@ test_that("window size", {
 
 test_that("loadTimeout", {
 
-  expect_success(ShinyDriver$new(test_path("apps/long-loading"), loadTimeout=20000))
+  ShinyDriver$new(test_path("apps/long-loading"), loadTimeout=20000)
 
 })
