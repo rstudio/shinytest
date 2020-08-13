4e88d014793e971893ba81003629401420f8137c
Hadley Wickham <h.wickham@gmail.com>
2020-08-13 15:48:53 -0500

    Skip tests that will always fail on windows

--- tests/testthat/test-recorded-tests.R
+++ tests/testthat/test-recorded-tests.R
@@ -5,10 +5,12 @@ test_that("pre-recorded tests still pass", {
     testApp(test_path(x), compareImages = FALSE, interactive = FALSE, quiet = TRUE)
   }
 
-  expect_pass(test_app("recorded_tests/009-upload"))
   expect_pass(test_app("recorded_tests/041-dynamic-ui"))
-  expect_pass(test_app("recorded_tests/app-waitForValue"))
   expect_pass(test_app("recorded_tests/inline-img-src"))
+
+  skip_on_os("windows")
+  expect_pass(test_app("recorded_tests/009-upload")) # file size different
+  expect_pass(test_app("recorded_tests/app-waitForValue")) # plot coords different
 
   skip_on_os("linux") # recorded on mac
   skip_on_ci() # fails for unknown reasons currently
