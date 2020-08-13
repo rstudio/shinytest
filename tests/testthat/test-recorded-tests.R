9ebb4d1445bd44631e5f3e1d92a9e26759c8939d
Hadley Wickham <h.wickham@gmail.com>
2020-08-13 07:27:38 -0500

    Simplify test a little more

--- tests/testthat/test-recorded-tests.R
+++ tests/testthat/test-recorded-tests.R
@@ -3,6 +3,6 @@ apps <- dir(test_path("recorded_tests"), full.names = TRUE)
 for (app in apps) {
   test_that(paste0("Pre-rendered app '", basename(app), "' works"), {
     skip_on_os("windows")
-    shinytest::expect_pass(shinytest::testApp(app, compareImages = FALSE))
+    expect_pass(testApp(app, compareImages = FALSE, interactive = FALSE))
   })
 }
