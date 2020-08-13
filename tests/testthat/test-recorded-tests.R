7f58fc872f8444c0e05385ce486f6bf17ab909bf
Hadley Wickham <h.wickham@gmail.com>
2020-08-13 07:49:05 -0500

    Also quiet tests

--- tests/testthat/test-recorded-tests.R
+++ tests/testthat/test-recorded-tests.R
@@ -3,6 +3,6 @@ apps <- dir(test_path("recorded_tests"), full.names = TRUE)
 for (app in apps) {
   test_that(paste0("Pre-rendered app '", basename(app), "' works"), {
     skip_on_os("windows")
-    expect_pass(testApp(app, compareImages = FALSE, interactive = FALSE))
+    expect_pass(testApp(app, compareImages = FALSE, interactive = FALSE, quiet = TRUE))
   })
 }
