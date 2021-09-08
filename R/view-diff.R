b4c9dbf38c54d3aed95dd487f61a49640f312df3
Barret Schloerke <schloerke@gmail.com>
2021-09-08 14:00:42 -0400

    Add support for `txt` file extensions

--- R/view-diff.R
+++ R/view-diff.R
@@ -41,7 +41,7 @@ diffviewer_widget <- function(old, new, width = NULL, height = NULL,
     bin_data <- read_raw(filename)
 
     # Assume .json and .download files are text
-    if (grepl("\\.json$", filename) || grepl("\\.download$", filename)) {
+    if (grepl("\\.(json|download|txt)$", filename)) {
       raw_to_utf8(bin_data)
     } else if (grepl("\\.png$", filename)) {
       paste0("data:image/png;base64,", jsonlite::base64_enc(bin_data))
