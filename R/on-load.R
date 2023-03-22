a73d042041b750c64b2c33fdc89a06ff5c708c30
Winston Chang <winston@stdout.org>
2023-03-22 16:36:17 -0500

    Move packageStartupMessage() out of .onLoad

--- R/on-load.R
+++ R/on-load.R
@@ -6,8 +6,16 @@
 
   # This will issue a message if phantomjs isn't found, converting the regular
   # message to a packageStartupMessage.
+  convert_message_to_package_startup_message({
+    find_phantom()
+  })
+}
+
+# Evaluate an expression, and if it emits any messsages, convert them to
+# packageStartupMessage.
+convert_message_to_package_startup_message <- function(expr) {
   withCallingHandlers(
-    find_phantom(),
+    force(expr),
     message = function(cnd) {
       packageStartupMessage(conditionMessage(cnd))
       tryInvokeRestart("muffleMessage")
